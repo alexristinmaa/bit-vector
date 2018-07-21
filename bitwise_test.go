d8ad3b5bca4f7f86eb7720c90afd862fad950c87
MaTriXTheCow <alexander.ristinmaa@icloud.com>
2018-07-21 15:06:00 +0200

    Create bitwise_test.go

---
+++ bitwise_test.go
@@ -0,0 +1,46 @@
+package bitvector
+
+import "testing"
+
+// TestAnd tests the and function
+func TestAnd(t *testing.T) {
+  a := new(BitVector).Add(1,3,5,7)
+  b := new(BitVector).Add(0,2,4,7)
+  a.And(b)
+  if a.AsBinary() != "0000000000000000000000000000000000000000000000000000000010000000" {
+    t.Error("Expected 0000000000000000000000000000000000000000000000000000000010000000, got", a.AsBinary())
+  }
+}
+
+// TestOr tests the or function
+func TestOr(t *testing.T) {
+  a := new(BitVector).Add(1,3,5,7)
+  b := new(BitVector).Add(0,2,4,6)
+  a.Or(b)
+  if a.AsBinary() != "0000000000000000000000000000000000000000000000000000000011111111" {
+    t.Error("Expected 0000000000000000000000000000000000000000000000000000000011111111, got", a.AsBinary())
+  }
+}
+
+// TestXor tests the xor function
+func TestXor(t *testing.T) {
+  a := new(BitVector).Add(0,2,3,4)
+  b := new(BitVector).Add(0,1,2,3)
+
+  a.Xor(b)
+
+  if a.AsBinary() != "0000000000000000000000000000000000000000000000000000000000010010" {
+    t.Error("Expected 0000000000000000000000000000000000000000000000000000000000010010, got", a.AsBinary())
+  }
+}
+
+// TestClear tests the clear function
+func TestClear(t *testing.T) {
+  a := new(BitVector).Add(0,1,2,3,4,5,6,7,8,9)
+
+  a.Clear()
+
+  if *a != 0 {
+    t.Error("Expected 0000000000000000000000000000000000000000000000000000000000010010, got", a.AsBinary())
+  }
+}
